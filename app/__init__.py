df58588461e6d447e2f41058f9bf9f25e910b143
Dmitry Murzin <dmitrymurzin91@gmail.com>
2020-06-24 17:44:11 +0500

    Add migrations files

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+db = SQLAlchemy(app) # represent database object
+migrate = Migrate(app, db) # represent migration mechanism
 
-from app import routes
+from app import routes, models
+# models determines bd structure
